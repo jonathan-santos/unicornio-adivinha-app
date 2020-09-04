867dbca14964bb25f4877bd49a05b5e40c3666b1
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-09-04 18:19:31 -0300

    Filled Home page with content and styles, added image unicorn to assets

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text, TouchableHighlight } from 'react-native'
 
+import UnicornImg from '../../assets/unicorn.png'
 import styles from './styles'
 
 const Home = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Home</Text>
+			<View style={styles.iconContainer}>
+				<Image style={styles.icon} source={UnicornImg} />
+			</View>
+
+			<View style={styles.navigationContainer}>
+				<View style={styles.navigationHorizontalLine}></View>
+				<Text style={{ ...styles.navigationIndicator, ...styles.navigationIndicatorSelected }}>1</Text>
+				<Text style={styles.navigationIndicator}>2</Text>
+				<Text style={styles.navigationIndicator}>3</Text>
+			</View>
+
+			<View style={styles.main}>
+				<Text style={styles.text}>Envie uma foto e nossos unicórnions (mix de unicórnio com minions) vão revelar o sentimento da pessoa na foto</Text>
+				<Text style={styles.text}>Vamos revelar o humor de uma pessoa utilizando apenas uma foto!</Text>
+				<TouchableHighlight style={styles.button}>
+					<Text style={styles.buttonText}>Começar</Text>
+				</TouchableHighlight>
+			</View>
 		</View>
 	)
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -4,7 +4,79 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    backgroundColor: '#4C3470'
+  },
+  iconContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#331E54'
+  },
+  icon: {
+    width: 100,
+    marginLeft: 16,
+    resizeMode: 'center'
+  },
+  navigationContainer: {
+    width: '90%',
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  navigationHorizontalLine: {
+    height: 4,
+    width: '100%',
+    position: 'absolute',
+    top: '50%',
+    backgroundColor: '#331E54'
+  },
+  navigationIndicator: {
+    height: 64,
+    width: 64,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#331E54',
+    color: '#FFFFFF',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 28
+  },
+  navigationIndicatorSelected: {
+    backgroundColor: '#00C9A8'
+  },
+  main: {
+    borderRadius: 16,
+    padding: 20,
+    backgroundColor: '#331E54'
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontFamily: 'sans-serif',
+    marginBottom: 24
+  },
+  button: {
+    borderRadius: 16,
+    padding: 16,
+    alignSelf: 'center',
+    backgroundColor: '#00C9A8',
+    color: '#FFFFFF'
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontFamily: 'sans-serif',
   }
 })
 
