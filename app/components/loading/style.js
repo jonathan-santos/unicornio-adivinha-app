91d64d4c6ff80c684eaf961c0da6c8862c2a4e29
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-07-16 11:22:17 -0300

    update app to updated expo and react-native versions

---
+++ app/components/loading/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  loading: {
+    marginBottom: 24
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontFamily: 'sans-serif',
+    // fontFamily: 'Montserrat_400Regular'
+  },
+  
+})
+
+export default styles
